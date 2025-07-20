ba7a7f153f9b82fb85725cb8bdffab689e5c8ddc
mdsalathossain0 <mdsalathossain0@gmail.com>
2025-07-20 13:25:18 +0600

    edit update done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
   let [input, setInput] = useState('')
   let [allitem, setAllitem] = useState([])
   let [message, setMessage] = useState(false)
+  let [update, setUpdate] = useState(false)
+  let [idStore, setIdStore] = useState('')
 
 
   let handleClick = () => {
@@ -27,6 +29,25 @@ const App = () => {
     remove(ref(db, 'alldata/' + item.id))
   }
 
+  let handleEdit = (item) =>{
+    setInput(item.name)
+    setUpdate(true)
+    setIdStore(item.id)
+  }
+
+  let handleUpdate = () =>{
+    console.log(idStore)
+    
+      set(ref(db, 'alldata/' + idStore), {
+      name: input
+
+    }).then(()=>{
+      setUpdate(false)
+      setInput('')
+    })
+
+  }
+
   useEffect(() => {
     const starCountRef = ref(db, 'alldata/');
     onValue(starCountRef, (snapshot) => {
@@ -39,10 +60,17 @@ const App = () => {
   }, [])
 
   return (
-    <div className='bg-blue-200 w-full  lg:w-[500px] h-auto mx-auto drop-shadow-2xl py-5 px-8 lg:rounded-lg lg:mt-10'>
-      <h1 className='text-4xl font-semibold text-center py-5'>Crud Operation</h1>
-      <input value={input} onChange={(e) => setInput(e.target.value)} className='w-[70%] border border-gray-500 mr-5 rounded-[4px] pl-2 pr-8 py-1.5 mb-5' type="text" />
-      <button onClick={handleClick} className='bg-blue-500 py-2 px-3 rounded-[6px] text-white'>Add</button>
+    <div className='max-w-[1200px] mx-auto'>
+      <div className='bg-blue-200 w-[95%] lg:w-[500px] h-auto mx-auto lg:drop-shadow-xl py-5 px-8 rounded-b-lg lg:rounded-lg lg:mt-10'>
+      <h1 className='text-4xl font-semibold text-center pt-5 pb-12'>Crud Operation</h1>
+      <input value={input} onChange={(e) => setInput(e.target.value)} className='w-[70%] border border-gray-500 mr-3 rounded-[4px] pl-2 pr-8 py-1.5 mb-5' type="text" />
+      {
+        update 
+        ?
+        <button onClick={handleUpdate} className='bg-green-500 py-2 px-3 rounded-[6px] text-white'>Update</button>
+        :
+        <button onClick={handleClick} className='bg-blue-500 py-2 px-3 rounded-[6px] text-white'>Add</button>
+      }
       {
         message && <p className='text-red-600 pb-3'>*Please enter something</p>
       }
@@ -50,10 +78,11 @@ const App = () => {
         allitem.map(item=>(
           <p className=' pb-3 flex flex-col flex-wrap items-center border-b border-black last:border-0'>
           <span className='pt-1 pb-3'>{item.name}</span>
-          <span className='pb-2'><button onClick={()=> handleDelete(item)} className='bg-red-500 py-2 px-3 rounded-[6px] text-white mx-5'>Delete</button><button className='bg-blue-800 py-2 px-3 rounded-[6px] text-white'>Edit</button></span>
+          <span className='pb-2'><button onClick={()=> handleDelete(item)} className='bg-red-500 py-2 px-3 rounded-[6px] text-white mx-5'>Delete</button><button onClick={()=>handleEdit(item)} className='bg-blue-800 py-2 px-3 rounded-[6px] text-white'>Edit</button></span>
           </p>
         ))
       }
+    </div>
     </div>
   )
 }
